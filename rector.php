131f6dd02328f43732c17d4b4c9bdfd472ae7681
David Windell <david@outeredgeuk.com>
2023-10-23 15:27:55 +0100

    Create rector.php default for PHP 8.1

---
+++ rector.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
+use Rector\Php81\Rector\ClassConst\FinalizePublicClassConstantRector;
+use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->skip([
+        __DIR__ . '/vendor',
+
+        NewlineAfterStatementRector::class => [
+            '*.phtml',
+        ],
+
+        ExplicitBoolCompareRector::class,
+        FinalizePublicClassConstantRector::class,
+        RemoveAlwaysTrueIfConditionRector::class
+    ]);
+
+    $rectorConfig->fileExtensions([
+        'php',
+        'phtml'
+    ]);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE
+    ]);
+};
