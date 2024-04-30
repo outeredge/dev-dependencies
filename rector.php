64c1d62abff9672eb3114663ea819446e8c0a058
David Windell <david@outeredgeuk.com>
2024-04-30 12:49:20 +0000

    Bump rector PHP set list to 8.3

--- rector.php
+++ rector.php
@@ -17,7 +17,7 @@ function applyRectorDefaults(RectorConfig $rectorConfig) {
     ]);
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_83,
         SetList::DEAD_CODE,
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE
