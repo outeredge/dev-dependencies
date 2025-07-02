55a805760529ef51565d605dce6d9f2275fd4915
David Windell <david@outeredgeuk.com>
2025-07-02 16:07:12 +0100

    Remove FinalizePublicClassConstantRector
    No longer exists

--- rector.php
+++ rector.php
@@ -6,7 +6,6 @@ use Rector\Set\ValueObject\SetList;
 use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
 use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
-use Rector\Php81\Rector\ClassConst\FinalizePublicClassConstantRector;
 use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
 
 function applyRectorDefaults(RectorConfig $rectorConfig) {
@@ -39,7 +38,6 @@ function outeredgeRectorSkipDefaults() {
         ],
 
         ExplicitBoolCompareRector::class,
-        FinalizePublicClassConstantRector::class,
         JsonThrowOnErrorRector::class
     ];
 }
